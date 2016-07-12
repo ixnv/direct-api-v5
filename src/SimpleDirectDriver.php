765564ba578798171c370ec4a02c393f32e6a3c6
Гафаров Артур <a.gafarov@elama.ru>
2016-07-12 13:03:14 +0300

    RED-1056 реализовал метод обновления баннеров

--- src/SimpleDirectDriver.php
+++ src/SimpleDirectDriver.php
@@ -4,6 +4,7 @@ namespace eLama\DirectApiV5;
 
 use eLama\DirectApiV5\Dto;
 use eLama\DirectApiV5\Dto\Ad;
+use eLama\DirectApiV5\Dto\Ad\AdUpdateItem;
 use eLama\DirectApiV5\Dto\AdGroup\AdGroupTypesEnum;
 use eLama\DirectApiV5\Dto\Campaign;
 use eLama\DirectApiV5\Dto\Campaign\CampaignsSelectionCriteria;
@@ -19,6 +20,7 @@ use eLama\DirectApiV5\RequestBody\GetAdsRequestBody;
 use eLama\DirectApiV5\RequestBody\GetCampaignsRequestBody;
 use eLama\DirectApiV5\RequestBody\GetKeywordsRequestBody;
 use eLama\DirectApiV5\RequestBody\GetRequestBody;
+use eLama\DirectApiV5\RequestBody\UpdateAdRequestBody;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise\PromiseInterface;
 use JMS\Serializer\Serializer;
@@ -28,14 +30,18 @@ class SimpleDirectDriver
 {
     /** @var DtoAwareDirectDriver  */
     private $driver;
-    /**
-     * @var null
-     */
+
+    /** @var int|null */
     private $pageLimit;
 
     /**
      * @param Serializer $jmsSerializer
      * @param Client $client
+     * @param LoggerInterface $logger
+     * @param string $baseUrl
+     * @param string $token
+     * @param string $login
+     * @param int|null $pageLimit
      */
     public function __construct(
         Serializer $jmsSerializer,
@@ -89,6 +95,7 @@ class SimpleDirectDriver
 
     /**
      * Получить незаархивированные текстовые объявления
+     * @param int[] $campaignIds
      * @return PromiseInterface
      * @see \eLama\DirectApiV5\Dto\Ad\AdGetItem
      */
@@ -159,12 +166,19 @@ class SimpleDirectDriver
     }
 
     /**
-     * @deprecated
      * @param Ad\AdUpdateItem[] $ads
+     * @return PromiseInterface
+     * @see UpdateResult
      */
     public function updateAds(array $ads)
     {
-        // TODO: реализовать метод
+        \Assert\thatAll($ads)->isInstanceOf(AdUpdateItem::class);
+
+        $request = new UpdateAdRequestBody(
+            new Ad\UpdateRequest($ads)
+        );
+
+        return $this->driver->call($request);
     }
 
     /**
