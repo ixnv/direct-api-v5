4051a6a9a1a3b69c924685c530e426ce4682eafa
gotovsky <skygsn@gmail.com>
2016-09-22 15:39:06 +0300

    Тесты

--- tests/Integration/DtoDirectDriver/BidsTest.php
+++ tests/Integration/DtoDirectDriver/BidsTest.php
@@ -5,7 +5,10 @@ namespace eLama\DirectApiV5\Test\Integration\DtoDirectDriver;
 use eLama\DirectApiV5\Dto\Bids\BidSetItem;
 use eLama\DirectApiV5\Dto\Bids\SetRequest;
 use eLama\DirectApiV5\Dto\General\AddResponseBody;
+use eLama\DirectApiV5\Dto\Keyword\AddRequest;
+use eLama\DirectApiV5\Dto\Keyword\KeywordAddItem;
 use eLama\DirectApiV5\DtoDirectDriver;
+use eLama\DirectApiV5\RequestBody\AddKeywordRequestBody;
 use eLama\DirectApiV5\RequestBody\SetBidsRequestBody;
 
 class BidsTest extends AdGroupExistenceDependantTestCase
@@ -25,15 +28,25 @@ class BidsTest extends AdGroupExistenceDependantTestCase
      */
     public function setBids()
     {
-        $requestBody = new SetBidsRequestBody(
-            new SetRequest(
-                [(new BidSetItem())->setAdGroupId(self::$adGroupId)->setBid(1)->setContextBid(2)]
-            )
-        );
+        $keywordAddItem = new KeywordAddItem('тестовая фраза', self::$adGroupId);
+
+        $requestBody = new AddKeywordRequestBody(new AddRequest([$keywordAddItem]));
 
         /** @var AddResponseBody $responseBody */
         $responseBody = $this->driver->call($requestBody)->wait()->getUnserializedBody();
 
-        $result = $responseBody->getResult();
+        $keywordId = $responseBody->getResult()->getAddResults()[0]->getId();
+
+
+        $bidRequestBody = new SetBidsRequestBody(
+            new SetRequest(
+                [(new BidSetItem())->setKeywordId($keywordId)->setBid(300000)->setContextBid(300000)]
+            )
+        );
+
+        /** @var AddResponseBody $responseBody */
+        $bidResponseBody = $this->driver->call($bidRequestBody)->wait()->getUnserializedBody();
+
+        $result = $bidResponseBody->getResult();
     }
 }
