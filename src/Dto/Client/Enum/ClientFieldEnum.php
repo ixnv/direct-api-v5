91785ce721f288e0ef45b0f11725a892d9dd7eb2
Gafarov <a.gafarov@elama.ru>
2016-11-29 16:06:16 +0300

    ClientFieldEnum - значения констант

--- src/Dto/Client/Enum/ClientFieldEnum.php
+++ src/Dto/Client/Enum/ClientFieldEnum.php
@@ -9,21 +9,21 @@ class ClientFieldEnum extends BaseEnum
 {
     const __DEFAULT = 'AccountQuality';
 
-    const ACCOUNT_QUALITY = 'AccountQuality';
-    const ARCHIVED = 'Archived';
-    const CLIENT_ID = 'ClientId';
-    const CLIENT_INFO = 'ClientInfo';
-    const COUNTRY_ID = 'CountryId';
-    const CREATED_AT = 'CreatedAt';
-    const CURRENCY = 'Currency';
-    const GRANTS = 'Grants';
-    const LOGIN = 'Login';
-    const NOTIFICATION = 'Notification';
-    const OVERDRAFT_SUM_AVAILABLE = 'OverdraftSumAvailable';
-    const PHONE = 'Phone';
-    const REPRESENTATIVES = 'Representatives';
-    const RESTRICTIONS = 'Restrictions';
-    const SETTINGS = 'Settings';
-    const TYPE = 'Type';
-    const VAT_RATE = 'VatRate';
+    const AccountQuality = 'AccountQuality';
+    const Archived = 'Archived';
+    const ClientId = 'ClientId';
+    const ClientInfo = 'ClientInfo';
+    const CountryId = 'CountryId';
+    const CreatedAt = 'CreatedAt';
+    const Currency = 'Currency';
+    const Grants = 'Grants';
+    const Login = 'Login';
+    const Notification = 'Notification';
+    const OverdraftSumAvailable = 'OverdraftSumAvailable';
+    const Phone = 'Phone';
+    const Representatives = 'Representatives';
+    const Restrictions = 'Restrictions';
+    const Settings = 'Settings';
+    const Type = 'Type';
+    const VatRate = 'VatRate';
 }
